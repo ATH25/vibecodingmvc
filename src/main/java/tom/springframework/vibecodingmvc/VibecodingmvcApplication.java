cdb99d40dc2ff5adeca657b9f47f75493688136a
ATH25 <iamajuthomas@gmail.com>
2025-07-31 17:19:16 -0400

    Initialize Spring Boot project with Maven configuration

---
+++ src/main/java/tom/springframework/vibecodingmvc/VibecodingmvcApplication.java
@@ -0,0 +1,13 @@
+package tom.springframework.vibecodingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibecodingmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibecodingmvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tom/springframework/vibecodingmvc/VibecodingmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package tom.springframework.vibecodingmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibecodingmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
