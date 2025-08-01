e70126007fd805696b8826d90648d4ba892d1dcf
ATH25 <iamajuthomas@gmail.com>
2025-08-01 10:54:10 -0400

    Add Beer entity and update .gitignore

---
+++ src/main/java/tom/springframework/vibecodingmvc/entities/Beer.java
@@ -0,0 +1,48 @@
+package tom.springframework.vibecodingmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Version;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+
+}
