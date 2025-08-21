2bef094100ef700264d5316d84772cc16deedaf8
ATH25 <iamajuthomas@gmail.com>
2025-08-20 21:52:38 -0400

    Add comments to BeerRequestDto fields and update prompts for BeerController schema documentation

--- src/main/java/tom/springframework/vibecodingmvc/models/BeerRequestDto.java
+++ src/main/java/tom/springframework/vibecodingmvc/models/BeerRequestDto.java
@@ -8,7 +8,9 @@ import java.math.BigDecimal;
 
 public record BeerRequestDto(
         @NotBlank String beerName,
+        // style of the beer, ALE, PALE ALE, IPA, etc
         @NotBlank String beerStyle,
+        // Universal Product Code, a 13-digit number assigned to each unique beer product by the Federal Bar Association
         @NotBlank String upc,
         @PositiveOrZero Integer quantityOnHand,
         @DecimalMin(value = "0.0", inclusive = false) BigDecimal price
